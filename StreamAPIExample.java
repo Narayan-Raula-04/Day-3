e50335bdb894b0a8eab2a643ca41d3cfe7c1a9d2
Narayan-Raula-04 <narayanraula61@gmaail.com>
2025-12-24 18:01:47 +0530

    Added program

---
+++ StreamAPIExample.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class StreamAPIExample {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10, 20, 30, 40);
+
+        list.stream()
+                .filter(n -> n > 20)
+                .forEach(System.out::println);
+    }
+}
